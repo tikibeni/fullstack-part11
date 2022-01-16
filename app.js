f2f27d2901d97ae243194651be809ca1d15c2096
Benjamin Blinnikka <benjamin.blinnikka@hotmail.com>
2022-01-16 19:53:31 +0200

    Exercise 11.11

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('V1.1') // this could be improved with env-variable...
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
